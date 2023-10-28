a2c0010df77c6bfd9ad5643f9731ff32742e7c41
Radisa <Radisa@GABRIEL-ULTRAKI>
2023-10-28 20:35:16 +0200

    Add project files.

---
+++ DEUTCHLAND PROGRAMMEN/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DEUTCHLAND_PROGRAMMEN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::DEUTCHLAND_PROGRAMMEN.Properties.Resources.deutchlandflagge2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(-4, 350);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(810, 279);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Algerian", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 483);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(729, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HIGH QUALITY GERMANY PROGRAMME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 622);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DEUTCHLAND PROGRAMMEN/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEUTCHLAND_PROGRAMMEN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x, y, a;
+        Pen olovka = new Pen(Color.Aqua, 3);
+        Random rand = new Random();
+        int remaining = 0;
+        Point incrementPoint = new Point(0, 0);
+
+        private Point randomizeLabelPosition(Label label, int remaining)
+        {
+            int targetY = rand.Next(0, 800);
+            int targetX = rand.Next(0, 620);
+            //Console.WriteLine($"targetX: {targetX}, targetY: {targetY}");
+
+            int incrementX = (label.Left - targetX) / remaining;
+            int incrementY = (label.Top - targetY) / remaining;
+
+            return new Point(incrementX, incrementY); ;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (remaining != 0)
+            {
+                label1.Top += incrementPoint.Y;
+                label1.Left += incrementPoint.X;
+                remaining--;
+            }
+            else
+            {
+                remaining = 100;
+                incrementPoint = randomizeLabelPosition(label1, remaining);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X; y = e.Y;
+            Console.WriteLine(e.X + "   " + e.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            g.DrawLine(olovka, x, y, e.X, e.Y);
+        }
+    }
+}
