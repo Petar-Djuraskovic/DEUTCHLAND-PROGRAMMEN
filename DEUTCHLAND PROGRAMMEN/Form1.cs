07e15984cbb14dfd06c6559f714b45d8fc71582c
Petar Djura <pdjuraskovic@gmail.com>
2023-11-04 01:08:50 +0100

    DeutcheProgram neue feature verbringen!! yippie!!

--- DEUTCHLAND PROGRAMMEN/Form1.cs
+++ DEUTCHLAND PROGRAMMEN/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace DEUTCHLAND_PROGRAMMEN
 {
@@ -18,10 +20,11 @@ namespace DEUTCHLAND_PROGRAMMEN
         }
 
         int x, y, a;
-        Pen olovka = new Pen(Color.Aqua, 3);
+        Pen olovka = new Pen(Color.Black, 3);
         Random rand = new Random();
         int remaining = 0;
         (float X, float Y) incrementPoint;
+        bool crtanje = false;
 
         private (float X, float Y) randomizeLabelPosition(Label label, int remaining)
         {
@@ -32,7 +35,7 @@ namespace DEUTCHLAND_PROGRAMMEN
             float incrementX = (targetX - label.Left) / remaining;
             float incrementY = (targetY - label.Top) / remaining;
 
-            (float X, float Y) returnTuple = (incrementX, incrementY);
+                (float X, float Y) returnTuple = (incrementX, incrementY);
 
             return returnTuple;
         }
@@ -51,6 +54,12 @@ namespace DEUTCHLAND_PROGRAMMEN
                 incrementPoint = randomizeLabelPosition(label1, remaining);
                 Console.WriteLine(incrementPoint.ToString());
             }
+
+            if (richTextBox1.Visible)
+            {
+                richTextBox1.Top = Form1.MousePosition.Y - this.Location.Y;
+                richTextBox1.Left = Form1.MousePosition.X - this.Location.X;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -58,16 +67,54 @@ namespace DEUTCHLAND_PROGRAMMEN
 
         }
 
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            olovka.Color = colorDialog1.Color;
+            pictureBox3.BackColor = colorDialog1.Color;
+        }
+
+        private void pictureBox4_MouseDown(object sender, MouseEventArgs e)
         {
             x = e.X; y = e.Y;
             Console.WriteLine(e.X + "   " + e.Y);
+
+            if (radioButton2.Checked) { crtanje = true; }
         }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            Graphics g = CreateGraphics();
-            g.DrawLine(olovka, x, y, e.X, e.Y);
+            if (radioButton3.Checked) { richTextBox1.Visible = true; }
+            else { richTextBox1.Visible = false; }
+        }
+
+        private void pictureBox4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (crtanje)
+            {
+                Graphics g = pictureBox4.CreateGraphics();
+                int x2 = e.X, y2 = e.Y;
+
+                g.DrawLine(olovka, x, y, x2, y2);
+
+                x = x2; y = y2;
+            }
+        }
+
+        private void pictureBox4_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Graphics g = pictureBox4.CreateGraphics();
+                g.DrawLine(olovka, x, y, e.X, e.Y);
+            }
+
+            crtanje = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            olovka.Width = (float)numericUpDown1.Value;
         }
     }
 }
